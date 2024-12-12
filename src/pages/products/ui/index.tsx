b83bf53ffef650d89c3165b52259e4b5d4d6dc9b
Stepan Ryndin <step.rindin@yandex.ru>
2024-12-12 23:24:12 +0300

    fix:

--- src/pages/products/ui/index.tsx
+++ src/pages/products/ui/index.tsx
@@ -1,15 +1,23 @@
 import { useEffect, useState } from "react";
 import { FavoriteFilter } from "../../../features";
 import { CardList } from "../../../widgets";
-import { loadProductsFromLocalStorage } from "../../../shared/utils/localStorageService";
+import { loadProductsFromLocalStorage, saveProductsToLocalStorage } from "../../../shared/utils/localStorageService";
 import { useAppSelector } from "../../../app/store/hooks";
+import { fetchProducts } from "../../../shared/api/api";
 
 export const Products = () => {
   const products = useAppSelector((state) => state.product.products);
   const [sohwFavorite, setShowFavorite] = useState(false);
 
   useEffect(() => {
-    loadProductsFromLocalStorage();
+    const localProduct = async () => {
+        const localProduct = loadProductsFromLocalStorage();
+        if (localProduct.length === 0) {
+          const serverProduct = await fetchProducts();
+          saveProductsToLocalStorage(serverProduct);
+        }
+    }
+    localProduct();
   }, []);
 
   const handleToggleFavorite = () => {
