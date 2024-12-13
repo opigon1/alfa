481e88aa56e1a7fd91e4e37e789e7d82b8f0cd61
Stepan <step.rindin@yandex.ru>
2024-12-13 11:28:17 +0300

    fix: initial-card-fix

--- src/pages/products/ui/index.tsx
+++ src/pages/products/ui/index.tsx
@@ -3,42 +3,39 @@ import styles from './index.module.scss'
 import { FavoriteFilter } from "../../../features";
 import { CardList } from "../../../widgets";
 import { loadProductsFromLocalStorage, saveProductsToLocalStorage } from "../../../shared/utils/localStorageService";
-import { useAppDispatch } from "../../../app/store/hooks";
+import { useAppDispatch, useAppSelector } from "../../../app/store/hooks";
 import { fetchProducts } from "../../../shared/api/api";
-import { setProducts } from "../../../entities/product/model/slices/slice"; // Если вы хотите использовать Redux
-import { IProduct } from "../../../entities/product/model/card";
+import { setProducts } from "../../../entities/product/model/slices/slice";
 
 export const Products = () => {
   const dispatch = useAppDispatch();
+  const products = useAppSelector((state) => state.product.products);
   const [isLoading, setIsLoading] = useState(true);
   const [showFavorite, setShowFavorite] = useState(false);
-  const [loadedProducts, setLoadedProducts] = useState<IProduct[]>([]); // Локальное состояние для продуктов
 
   useEffect(() => {
     const initializeProducts = async () => {
       setIsLoading(true);
       const localProductData = loadProductsFromLocalStorage();
-      if (localProductData.length === 0) {
+      if (!localProductData || localProductData.length === 0) {
         const serverProduct = await fetchProducts();
         saveProductsToLocalStorage(serverProduct);
-        setLoadedProducts(serverProduct); // Обновляем локальное состояние
-        dispatch(setProducts(serverProduct)); // Если вы используете Redux
+        dispatch(setProducts(serverProduct));
       } else {
-        setLoadedProducts(localProductData); // Если данные уже есть в localStorage
-        dispatch(setProducts(localProductData)); // Если вы используете Redux
+        dispatch(setProducts(localProductData));
       }
       setIsLoading(false);
     };
-    initializeProducts ();
-  }, [dispatch]); // Пустой массив зависимостей, чтобы эффект выполнился только один раз
+    initializeProducts();
+  }, [dispatch]);
 
   const handleToggleFavorite = () => {
     setShowFavorite(!showFavorite);
   };
 
   const filteredProducts = showFavorite
-    ? loadedProducts.filter((product) => product.favorite)
-    : loadedProducts;
+    ? products.filter((product) => product.favorite)
+    : products;
 
   return (
     <>
